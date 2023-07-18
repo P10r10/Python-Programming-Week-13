a3cc99d170151c8100b58c9c902743195d822c04
P10r10 <asantiago@keemail.me>
2023-07-18 15:02:36 +0100

    main

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ window = pygame.display.set_mode((640, 480))
 
 while True:
     for event in pygame.event.get():
-        print(event)
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            print("you pressed the button number",
+                  event.button, "at location", event.pos)
+
         if event.type == pygame.QUIT:
             exit()
